39546ea0bf89299f4b112ca3d45126eb6881f59c
Stefan Ober <StefanO(a)ppedv.de>
2021-10-07 13:42:25 +0200

    Skript mit CommentBasedHelp

--- 02-Skripting/Get-logonevents.ps1
+++ 02-Skripting/Get-logonevents.ps1
@@ -1,4 +1,37 @@
-﻿[cmdletBinding()]
+﻿<#
+.Synopsis
+   Skript zum Abfragen von LogOn / LogOff Events
+.DESCRIPTION
+   Mit diesem Skript können Sie auf dem lokalen oder auf einem Remote System folgende Arten von Events abfragen.
+   Anmeldeevent
+   Abmeldeevent
+   fehlgeschlagene Anmeldung
+.EXAMPLE
+   Get-logonevents.ps1 -EventId 4624
+
+   Bei der Verwendung mit EventID 4624 werden die Anmeldeevents abgefragt. Default werden die aktuellesten 5 Events ausgegeben.
+
+   Index Time          EntryType   Source                 InstanceID Message
+   ----- ----          ---------   ------                 ---------- -------
+   19500 Okt 07 13:35  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+   19497 Okt 07 13:35  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+   19494 Okt 07 13:35  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+   19490 Okt 07 13:34  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+   19486 Okt 07 13:34  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+
+
+.EXAMPLE
+   Ein weiteres Beispiel für die Verwendung dieses Cmdlets
+.PARAMETER EventId
+    Mit diesem Parameter wird die zu abfragende EventID festgelegt.
+    4624 Anmeldung
+    4625 fehlgeschlagene Anmeldung
+    4634 Abmeldung
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1#comment-based-help-keywords
+Doku zu Comment based Help
+#>
+[cmdletBinding()]
 param(
 [string]$Computername = "localhost",
 
