b07aa1722cb2be354d92ab5b6900531635755a57
Stefan Ober <StefanO(a)ppedv.de>
2021-10-07 13:30:22 +0200

    Skripting - Parametrisiert und mit Verbose

---
+++ 02-Skripting/Get-logonevents.ps1
@@ -0,0 +1,12 @@
+﻿[cmdletBinding()]
+param(
+[string]$Computername = "localhost",
+
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[int]$Newest = 5
+)
+
+Write-Verbose -Message "ZusatzInfo: Vom User wurde folgende EventID übergeben: $EventId"
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
