e5da049a3b69077760486bc6b02fd17b0288c013
Stefan Ober <StefanO(a)ppedv.de>
2021-10-08 14:39:46 +0200

    Fehlerbehandlung

---
+++ 07-Fehlerbehandlung/Write-Debug.ps1
@@ -0,0 +1,13 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$text
+)
+Write-Debug -Message "Vor erster Ausgabe"
+Write-Host -object "1. $text" -ForegroundColor Magenta
+
+Write-Debug -Message "Vor zweiter Ausgabe aber nach erster"
+Write-Host -object "2. $text" -ForegroundColor Cyan
+
+Write-Debug -Message "Vor dritter Ausgabe aber nach zweiter"
+Write-Host -object "3. $text" -ForegroundColor Green
